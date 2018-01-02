b58db1ec22e769c5fd0690aeda9ded9d484571c2
Ace <33731173+ace-trainer-void@users.noreply.github.com>
2018-01-01 20:32:17 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,11 +12,11 @@ client.on("message", (message) => {
 
 
     if (command === "dex") {
-        if (args[0] === "dowlputy") {
-            message.channel.send("**Name:** Dowlputy\n**Dex Number:** 001\n**Types:** Flying\n**Species:** The Deputy Pokemon\n**Height:** 2'3\n**Weight:** 16 lbs\n**Stats:** [stats]\n**Egg Group:** Flying, Field\n**Dex Entry:** `[dex entry]`\nhttps://78.media.tumblr.com/1fc558f530b981f72f4245b566191422/tumblr_ozj7lrCTvk1wfcogao1_1280.png");
+        if (args[0] === "Pokémon Name") {
+            message.channel.send("**Name:** [Name]\n**Dex Number:** 001\n**Types:** Grass\n**Species:** [Species Name]\n**Height:** [Height]\n**Weight:** [Weight]\n**Stats:** [stats]\n**Egg Group:** [Egg Group]\n**Dex Entry:** `[dex entry]`");
         } else
-        if (args[0] === "owlficer") {
-            message.channel.send("**Name:** Owlficer\n**Dex Number:** 002\n**Types:** Flying/Ghost\n**Species:** The Lookout Pokemon**\nHeight:** 3'6\n**Weight:** 95 lbs\n**Stats:** [stats]\n**Egg Group:** Flying, Field\n**Dex Entry:** `[dex entry]`\nhttps://78.media.tumblr.com/d1462bf64a6a47ed6839b6a6ae6e2be0/tumblr_ozj7lrCTvk1wfcogao2_1280.png");
+        if (args[0] === "Pokémon Name") {
+            message.channel.send("**Name:** [Name]\n**Dex Number:** 002\n**Types:** Grass\n**Species:** [Species]**\nHeight:** [Height]\n**Weight:** [Weight]\n**Stats:** [stats]\n**Egg Group:** [Egg Group]\n**Dex Entry:** `[dex entry]`");
         } else
         if (args[0] === "eaglective") {
             message.channel.send("**Name:** Eaglective\n**Dex Number:** 003\n**Types:** Flying/Ghost\n**Species:** The Gumshoe Pokemon\n**Height:** 6'0\n**Weight:** 140 lbs\n**Stats:** [stats]\n**Egg Group:** Flying, Field\n**Dex Entry:** `[dex entry]`\nhttps://78.media.tumblr.com/bc8060d13f9ba7e4e98bcf12e41b4f2d/tumblr_ozj7lrCTvk1wfcogao3_1280.png");
